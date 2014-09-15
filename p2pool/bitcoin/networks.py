855ff87d9b06b00e078722619b0b4c96e6a586a6
TheKnightOrc <knightorc@xqqme.com>
2014-09-15 04:46:41 +0000

    Change block explorer

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -142,9 +142,9 @@ nets = dict(
         BLOCK_PERIOD=60, # s
         SYMBOL='YUM',
         CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Candycoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Candycoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.candycoin'), 'candycoin.conf'),
-        BLOCK_EXPLORER_URL_PREFIX='http://wade01.xqqme.com:2750/block/',
-        ADDRESS_EXPLORER_URL_PREFIX='http://wade01.xqqme.com:2750/address/',
-        TX_EXPLORER_URL_PREFIX='http://wade01.xqqme.com:2750/tx/',
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.candyco.in:2750/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.candyco.in:2750/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.candyco.in:2750/tx/',
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
